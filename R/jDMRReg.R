658f5d54421b216be0e84fc735929fda5d820287
robert-piecyk <robert.s.piecyk@gmail.com>
2023-07-12 18:07:44 +0200

    Fix errors #16

--- R/jDMRReg.R
+++ R/jDMRReg.R
@@ -136,7 +136,7 @@ modifiedExportMethylome <- function(model, out.dir, context, name) {
     final_dataset$rc.meth.lvl <- floorDec(as.numeric(as.character(final_dataset$rc.meth.lvl)),5)
     final_dataset$seqnames <- as.character(final_dataset$seqnames)
 
-    saveFile <- paste0(out.dir, "/", name, "_", context, ".txt")
+    saveFile <- paste0(out.dir, "/", basename(name), "_", context, ".txt")
     fwrite(final_dataset, file = saveFile, quote = FALSE, sep = '\t', row.names = FALSE, col.names = TRUE)
     return (final_dataset)
 }
